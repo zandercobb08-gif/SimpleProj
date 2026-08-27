d5bcb7f86280a5674d3ea1f59162524c47684790
zandercobb08-gif <zandercobb08@gmail.com>
2026-08-27 15:29:37 -0700

    added math

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
-
+int powered;
 int main()
 {
+
 	std::cout << "did it work" << std::endl;
 	std::cout << "Close enough" << std::endl;
+	std::cin >> powered;
+	powered = powered + 1;
+	std::cout << powered << std::endl;
 	return 0;
 }
