f1b3d84e42dc83853c108e6a911f8c3f0b59907f
zandercobb08-gif <zandercobb08@gmail.com>
2026-08-28 15:46:18 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,70 +1,194 @@
 #include <iostream>
+#include <cstdlib>
 int grade;
 std::string password;
 std::string username;
 float HP;
 float heat;
+
+
+void lockpick() 
+{
+	int guess;
+	int lock = rand() % 99;
+	bool open = false;
+	int guesses = 0;
+
+	bool unsolved = true;
+	while (!open) {
+		std::cout << "What is the keypad code? (Pick a number 1-100)" << std::endl;
+		std::cin >> guess;
+		if (guess == lock)
+			open = true;
+		else if (guess > lock)
+			std::cout << "Too high" << std::endl;
+		else if (guess < lock)
+			std::cout << "Too low" << std::endl;
+		else
+			std::cout << "Something did not work correctly, go find out what" << std::endl;
+
+
+
+	}
+
+	while (open && unsolved) {
+		unsolved = false;
+		std::cout << "You got the right code" << std::endl;
+	}
+
+
+
+
+}
+void rockmine() {
+	int stamina = 5;
+	while (stamina > 0) {
+		int success = rand() % 5;
+		if (success == 0)
+			std::cout << "You obtain nothing" << std::endl;
+		else
+			if (success == 4)
+				std::cout << "You obtain 2 chunks of wyvern obsidian" << std::endl;
+			else
+				std::cout << "You obtain a chunk of wyvern obsidian" << std::endl;
+		stamina = stamina - 1;
+
+
+
+
+	}
+}
+void attacksim() {
+	for (int turncount = 0; turncount <= 5; turncount++) {
+		int crit = rand() % 5;
+		if (crit == 1)
+			std::cout << "You attacked! It was a Crit!" << std::endl;
+		else
+			std::cout << "You attacked!" << std::endl;
+
+
+
+
+	}
+}
 int main()
 {
+	bool playing = true;
+	int keepplaying;
+	while (playing) 
+	{
+		int game;
+		std::cout << "Choose which program to run" << std::endl;
+		std::cout << "press 1 to play the attack simulator" << std::endl;
+		std::cout << "press 2 to play the mining simulator" << std::endl;
+		std::cout << "press 3 to play the lockpicking game" << std::endl;
+		std::cin >> game;
+		if (game == 1)
+			attacksim();
+		else if (game == 2)
+			rockmine();
+		else if (game == 3)
+			lockpick();
+		else
+			std::cout << "That is an invalid game" << std::endl;
+		std::cout << "Play another game? 1 for y or 2 for n" << std::endl;
+		std::cin >> keepplaying;
+	if (keepplaying == 1)
+		std::cout << "OK" << std::endl;
+	else if (keepplaying == 2) {
+		std::cout << "OK, have a nice day" << std::endl;
+		playing = false;
+		}
+		
+	}
+
+	return 0;
 
 
-	//std::cout << "What's your grade?" << std::endl;
-	//std::cin >> grade;
-	//std::cout << grade << std::endl;
-	//if (grade >= 90)
-	//	std::cout << "An A" << std::endl;
-	//else if (grade >= 80)
-	//	std::cout << "A B" << std::endl;
-	//else if (grade >= 70)
-	//	std::cout << "A C" << std::endl;
-	//else if (grade >= 60)
-	//	std::cout << "A D" << std::endl;
-	//else if (grade < 60)
-	//	std::cout << "An F" << std::endl;
-	//else
-	//	std::cout << "Thats not a valid score" << std::endl;
-
-
-	//std::cout << "Give me a whole number" << std::endl;
-	//std::cin >> grade;
-	//if (grade == 0)
-	//	std::cout << "You chose 0 of all numbers" << std::endl;
-	//else if (grade % 2 == 1)
-	//	std::cout << "An odd number" << std::endl;
-	//else
-	//	std::cout << "an even number" << std::endl;
-
-	std::cout << "Username" << std::endl;
-	std::cin >> username;
-	std::cout << "password" << std::endl;
-	std::cin >> password;
-
-	if (username == "hello" && password == "1234")
-		std::cout << "access granted" << std::endl;
-	else
-		std::cout << "access denied" << std::endl;
-
-	//std::cout << "Give me a health percentage" << std::endl;
-	//std::cin >> HP;
-
-	//if (HP >= 75)
-	//	std::cout << "You don't need healing" << std::endl;
-	//else if (HP >= 50)
-	//	std::cout << "You should take a small health potion" << std::endl;
-	//else if (HP >= 0)
-	//	std::cout << "you should use a larger health potion" << std::endl;
-	//else
-	//	std::cout << "Your out of HP" << std::endl;
-
-	//std::cout << "how hot is it today" << std::endl;
-	//std::cin >> heat;
-	//if (heat >= 80)
-	//	std::cout << "A pair of shorts and a tshirt should work" << std::endl;
-	//else if (heat >= 60)
-	//	std::cout << "A long sleeve shirt and some jeans sound nice" << std::endl;
-	//else if (heat >= 32)
-	//	std::cout << "you should were something pretty warm" << std::endl;
-	//else
-	//	std::cout << "You need a thick jacket today" << std::endl;
-	//return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*std::cout << "What's your grade?" << std::endl;
+std::cin >> grade;
+std::cout << grade << std::endl;
+if (grade > 100)
+	std::cout << "That's not possible" << std::endl;
+else if (grade >= 90)
+	std::cout << "An A" << std::endl;
+else if (grade >= 80)
+	std::cout << "A B" << std::endl;
+else if (grade >= 70)
+	std::cout << "A C" << std::endl;
+else if (grade >= 60)
+	std::cout << "A D" << std::endl;
+else if (grade < 60 && grade >= 0)
+	std::cout << "An F" << std::endl;
+else
+	std::cout << "Thats not a valid score" << std::endl;
+
+
+std::cout << "Give me a whole number" << std::endl;
+std::cin >> grade;
+if (grade == 0)
+	std::cout << "You chose 0 of all numbers" << std::endl;
+else if (grade % 2 == 1)
+	std::cout << "An odd number" << std::endl;
+else
+	std::cout << "an even number" << std::endl;
+
+std::cout << "Username" << std::endl;
+std::cin >> username;
+std::cout << "password" << std::endl;
+std::cin >> password;
+
+if (username == "hello" && password == "1234")
+	std::cout << "access granted" << std::endl;
+else
+	std::cout << "access denied" << std::endl;
+
+std::cout << "Give me a health percentage" << std::endl;
+std::cin >> HP;
+
+if (HP >= 75)
+	std::cout << "You don't need healing" << std::endl;
+else if (HP >= 50)
+	std::cout << "You should take a small health potion" << std::endl;
+else if (HP >= 0)
+	std::cout << "you should use a larger health potion" << std::endl;
+else
+	std::cout << "Your out of HP" << std::endl;
+
+std::cout << "how hot is it today" << std::endl;
+std::cin >> heat;
+if (heat >= 80)
+	std::cout << "A pair of shorts and a tshirt should work" << std::endl;
+else if (heat >= 60)
+	std::cout << "A long sleeve shirt and some jeans sound nice" << std::endl;
+else if (heat >= 32)
+	std::cout << "you should were something pretty warm" << std::endl;
+else
+	std::cout << "You need a thick jacket today" << std::endl;
+return 0;*/
+
