50c047d0eeb2b8af918a3f752416f39c7f1f8db8
zandercobb08-gif <zandercobb08@gmail.com>
2026-09-04 09:33:02 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,55 +1,23 @@
 #include <iostream>
 #include <cstdlib>
 using namespace std;
-
-struct character { string name; int hp; int level; };
-struct adress { string sector; string city; };
-struct player { string name; adress homeadress; int score; };
-class bankaccount
-{
-private: 
-	int balance = 20;
+enum enemy{GOBLIN, SKELETON, DEMON};
+enum weapon { CLUB, DAGGER, SWORD};
+class player { 
 public:
-	void deposit(int amount) {
-		balance += amount;
+	string name;
+	int hp;
+	void getname() {
+		cout << "What is this heros name?" << endl;
+		cin >> name;
+	}
+	void takedamage(int damage) {
 
 	}
-	int getbalance() {
-		return balance;
-	}
-
-
+	player() { name = "n / a"; hp = 100; }
 };
 
 int main()
 {
-	
-	character sif;
-	sif.name = "Siffrid";
-	sif.hp = 80;
-	sif.level = 24;
-	
-	cout << sif.name << " has " << sif.hp << " hp at level " << sif.level << endl;
-	
-	int deposit;
-	bankaccount myaccount;
-	cout << "You have " << myaccount.getbalance() << " dollars in your account." << endl;
-	cout << "How much would you like to deposit?" << endl;
-	cin >> deposit;
-	myaccount.deposit(deposit);
-	cout << "You now have " << myaccount.getbalance() << " dollars in your account" << endl;
 
-	player cor;
-	cor.name = "Corleon";
-	cor.homeadress.sector = "Empyrium";
-	cor.homeadress.city = "Ishgard";
-	cor.score = 90;
-
-	cout << cor.name << " lives in the " << cor.homeadress.sector << " in " << cor.homeadress.city << " with a score of " << cor.score << endl;
-
-
-
-
-	return 0;
 }
-
