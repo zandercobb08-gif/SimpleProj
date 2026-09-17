74b5b5264703750afb03fd28d74da1bfcbb604b3
zandercobb08-gif <zandercobb08@gmail.com>
2026-09-17 11:09:52 -0700

    Save system refined

---
+++ Filefuncs.h
@@ -0,0 +1,70 @@
+#pragma once
+#include <fstream>
+#include "Utilities.h"
+void writetofile(string event) {
+ofstream logfile("test_results.txt");
+if (logfile.is_open()) {
+	logfile << event << endl;
+}
+logfile.close();
+}
+void appendtolog(string event) {
+	ofstream logfile("test_results.txt", ios::app);
+	if (logfile.is_open()) {
+		logfile << event << endl;
+		logfile.close();
+	}
+}
+bool fileexists(string filename) {
+	ifstream file(filename);
+	return file.is_open();
+}
+void fileop(string log) {
+	if (fileexists("test_results.txt")) {
+		appendtolog(log);
+	}
+	else
+		writetofile(log);
+}
+void firsthighscore(int event) {
+	ofstream logfile("highscore.txt");
+	if (logfile.is_open()) {
+		logfile << event << endl;
+	}
+	logfile.close();
+}
+void newhighscore(int event) {
+	ifstream datafile("highscore.txt");
+	int highscore = 0;
+	if (datafile.is_open()) {
+		datafile >> highscore;
+		datafile.close();
+	}
+	ofstream logfile("highscore.txt");
+	if (logfile.is_open()) {
+		if (event > highscore) {
+			logfile << event << endl;
+		}
+		else {
+			logfile << highscore << endl;
+		}
+
+		logfile.close();
+	}
+}
+void highscorefile(int score) {
+	if (fileexists("highscore.txt")) {
+		newhighscore(score);
+	}
+	else
+		firsthighscore(score);
+}
+int printhighscore() {
+	ifstream datafile("highscore.txt");
+	int highscore = 0;
+	if (datafile.is_open()) {
+		datafile >> highscore;
+		datafile.close();
+	}
+	return highscore;
+}
--- main.cpp
+++ main.cpp
@@ -1,74 +1,13 @@
-#include <fstream>
+
 #include <iostream>
 #include <cstdlib>
 #include "Utilities.h"
 #include "player.h"
 #include "enemy.h"
+#include "Filefuncs.h"
 
-void writetofile(string event) {
-	ofstream logfile("test_results.txt");
-	if (logfile.is_open()) {
-		logfile << event << endl;
-	}
-	logfile.close();
-}
-void appendtolog(string event) {
-	ofstream logfile("test_results.txt", ios::app);
-	if (logfile.is_open()) {
-		logfile << event << endl;
-		logfile.close();
-	}
-}
-bool fileexists(string filename) {
-	ifstream file(filename);
-	return file.is_open();
-}
-void fileop(string log) {
-	if (fileexists("test_results.txt")) {
-		appendtolog(log);
-	}
-	else
-		writetofile(log);
-}
-void firsthighscore(int event) {
-	ofstream logfile("highscore.txt");
-	if (logfile.is_open()) {
-		logfile << event << endl;
-	}
-	logfile.close();
-}
-void newhighscore(int event) {
-	ifstream datafile("highscore");
-	int highscore = 0;
-	if (datafile.is_open()) {
-		datafile >> highscore;
-		datafile.close();
-	}
-	ofstream logfile("highscore.txt");
-	if (logfile.is_open()) {
-		if (event > highscore) {
-			logfile << event << endl;
-		}
-		logfile.close();
-	}
-}
-void highscorefile(int score) {
-	if (fileexists("highscore.txt")) {
-		newhighscore(score);
-	}
-	else
-		firsthighscore(score);
-}
-int printhighscore() {
-	ifstream datafile("highscore");
-	int highscore = 0;
-	if (datafile.is_open()) {
-		datafile >> highscore;
-		cout << "Your highscore: " << highscore << endl;
-		datafile.close();
-	}
-	return highscore;
-}
+
+
 int main()
 {
 	
